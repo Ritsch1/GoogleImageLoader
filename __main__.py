461757cea8eca67da899f7814a83a2b6f4c15676
plesz <r.plesz@yahoo.de>
2021-06-12 00:43:15 +0200

    Added main script to be able to execute the application from the command line.

---
+++ __main__.py
@@ -0,0 +1,17 @@
+import GoogleImageLoader.Google_Image_Loader
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="Downloading images from google image search.")
+parser.add_argument("--keys" ,"-k", nargs="+", help="Search - keys to download images for.")
+parser.add_argument("--num_images", "-n", default="20", type=int, help="Maximum number of images to be downloaded for each search - key.")
+args = parser.parse_args()
+
+PREFIX = os.path.join(os.path.expanduser("~"), "GoogleImageLoads")
+gil = GoogleImageLoader.Loader(args.keys, args.num_images)
+gil.reformat_search_keys()
+gil.create_image_dirs()
+gil.scroll_through_google_images()
+url_queue = gil.extract_picture_urls()
+gil.download_images(url_queue=url_queue)
+print(f"Successfully downloaded images for the search - keys:\n{','.join(x for x in args.keys)}")
