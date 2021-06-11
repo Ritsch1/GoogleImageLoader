7a99cda55416ee060ed37bb3a8eda52d58301a5d
plesz <r.plesz@yahoo.de>
2021-06-11 17:12:10 +0200

    Add additional check to init method

--- src/Google_Image_Loader.py
+++ src/Google_Image_Loader.py
@@ -7,11 +7,14 @@ class GoogleImageLoader():
             search_keys : List of search keys for which images shall be downloaded.
                           If the list is empty, a ValueError is raised.
         """
-        if type(search_keys) != list:
-            raise ValueError(f"provided search_key {search_keys} is not a list.")
-        elif len(search_keys) == 0:
-            raise ValueError("No search_key was provided.")
+        if type(search_keys) != list or type(search_keys) != tuple:
+            raise ValueError(f"provided search_keys - value {search_keys} is not an iterable.")
+
         else:
+            # Removing empty strings
+            search_keys = [s for s in search_keys if len(s) > 0]
+            if len(search_keys) == 0:
+                raise ValueError("No search_key was provided.")
             #Remove duplicate search_keys
             search_keys = list(set(search_keys))
             self.search_keys = search_keys
