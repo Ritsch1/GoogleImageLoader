db454230f8e927ec91e3792b3cc09946f80b3cd2
plesz <r.plesz@yahoo.de>
2021-06-11 18:18:58 +0200

    Add tests for the Loader class.

--- Tests/test_loader.py
+++ Tests/test_loader.py
@@ -1,20 +1,20 @@
 import unittest
-import os
+from os import listdir, rmdir
 from GoogleImageLoader import Google_Image_Loader
 
-class GoogleImageLoaderTest(unittest.TestCase):
+class LoaderTest(unittest.TestCase):
 
     def test_init_list_two_keys(self):
         """
         Testing the __init__ method of the Google_Image_Loader - class
         """
-        search_keys = ["Hello World", "Nice to meet you"]
+        search_keys = ["Hello Mars", "Nice to meet you too"]
         gil = Google_Image_Loader.Loader(search_keys)
         self.assertIsInstance(gil, Google_Image_Loader.Loader)
 
     def test_init_tuple_one_key(self):
         """
-         Testing the __init__ method of the Google_Image_Loader - class
+        Testing the __init__ method of the Google_Image_Loader - class
         """
         search_keys = ("Hello World", "Nice to meet you")
         gil = Google_Image_Loader.Loader(search_keys)
@@ -22,31 +22,65 @@ class GoogleImageLoaderTest(unittest.TestCase):
 
     def test_init_tuple_no_key(self):
         """
-            Testing the __init__ method of the Google_Image_Loader - class
+        Testing the __init__ method of the Google_Image_Loader - class
         """
         search_keys = ()
-        self.assertRaises(ValueError, Google_Image_Loader.Loader(search_keys))
+        self.assertRaises(ValueError, Google_Image_Loader.Loader, search_keys)
 
     def test_init_list_no_key(self):
         """
-            Testing the __init__ method of the Google_Image_Loader - class
+        Testing the __init__ method of the Google_Image_Loader - class
         """
         search_keys = []
-        self.assertRaises(ValueError, Google_Image_Loader.Loader(search_keys))
+        self.assertRaises(ValueError, Google_Image_Loader.Loader, search_keys)
 
     def test_init_tuple_empty_key(self):
         """
-            Testing the __init__ method of the Google_Image_Loader - class
+        Testing the __init__ method of the Google_Image_Loader - class
         """
         search_keys = ("")
-        self.assertRaises(ValueError, Google_Image_Loader.Loader(search_keys))
+        self.assertRaises(ValueError, Google_Image_Loader.Loader, search_keys)
 
     def test_init_tuple_empty_key(self):
         """
-            Testing the __init__ method of the Google_Image_Loader - class
+        Testing the __init__ method of the Google_Image_Loader - class
         """
         search_keys = [""]
-        self.assertRaises(ValueError, Google_Image_Loader.Loader(search_keys))
+        self.assertRaises(ValueError, Google_Image_Loader.Loader, search_keys)
+
+    def test_create_image_dirs_no_search_keys(self):
+        """
+        Testing the create_image_dirs method of the Loader - class
+        """
+        search_keys = []
+        self.assertTrue(set(search_keys).issubset(set(listdir())))
+
+    def test_create_image_dirs_one_search_key_list(self):
+        """
+        Testing the create_image_dirs method of the Loader - class
+        """
+        # Arrange
+        search_keys = ["Dogs"]
+        gil = Google_Image_Loader.Loader(search_keys)
+        # Act
+        gil.create_image_dirs()
+        # Assert
+        self.assertTrue(set(search_keys).issubset(set(listdir())))
+        # Cleaning
+        for s in search_keys:
+            rmdir(s)
+
+    def test_create_image_dirs_two_search_keys_tuple(self):
+        # Arrange
+        search_keys = ("Cats", "Unicorns")
+        gil = Google_Image_Loader.Loader(search_keys)
+        # Act
+        gil.create_image_dirs()
+        # Assert
+        self.assertTrue(set(search_keys).issubset(set(listdir())))
+        # Cleaning
+        for s in search_keys:
+            rmdir(s)
 
 if __name__ == '__main__':
     unittest.main()
